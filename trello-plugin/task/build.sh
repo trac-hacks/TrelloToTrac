21c012ffee2750da172422654a8eb723e944fad4
Matteo Magni <matteo@magni.me>
2013-04-04 01:12:23 +0200

    sistemato task

--- trello-plugin/task/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-cd ..
-python setup.py bdist_egg
-echo '---------------------------copio------------------------------------------------'
-sudo cp dist/TrelloToTrac-0.1-py2.7.egg /var/enabu-env/python/lib/python2.7/site-packages/
-sudo service apache2 restart
-
